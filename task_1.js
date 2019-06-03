3d736e38b99adebee023c80e09aa25ae67b85067
Юсуф Алексей А <a_yusuf@ugrasu.ru>
2019-06-03 10:58:04 +0500

    Доработал логику с округлением

--- task_1.js
+++ task_1.js
@@ -4,12 +4,15 @@
             return {
                 num_1: 0.1,
                 num_2: 0.2,
-                result: null
+                result: null,
+                num: 15 // число знаков после запятой
             }
         },
         methods: {
             sum() {
-                this.result = Math.round((this.num_1 + this.num_2) * 1000) / 1000;
+                
+                var accuracy = Math.pow(10, this.num); // переменная, отвечающая за точность вычисления
+                this.result = Math.round((this.num_1 + this.num_2) * accuracy) /accuracy;
                 alert('Результат: ' + this.result);
             }
         }
